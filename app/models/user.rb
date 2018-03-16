37b7d5461139d97232678a05374474e41b4c11f9
PhucPham <pdkitf@gmail.com>
2018-03-17 01:44:55 +0700

    add user_name into user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  validates :email, presence: true, uniqueness: true
+
   def like? post_id
     likes.find_by_post_id(post_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
   resources :likes, only: [:create, :destroy]
 end
