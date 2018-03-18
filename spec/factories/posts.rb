99b3b41b2c72dfe9ff3b056fbe1ef5b55fe68c53
PhucPham <pdkitf@gmail.com>
2018-03-19 01:10:41 +0700

    test post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :post, class: Post do
+    title { Faker::Lorem.sentence }
+    image Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/public/apple-touch-icon.png')))
+    association :user, factory: :user
+  end
 
+  factory :invalid_post, parent: :post do
+    image nil
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.build(:post) }
+
+  describe 'validation' do
+    context 'invalid attributes' do
+      subject { FactoryBot.build :invalid_user }
+      it { expect(subject).to be_invalid }
+    end
+
+    it 'valid attributes' do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'association' do
+    it 'belongs to user' do
+      relation = described_class.reflect_on_association(:user)
+      expect(relation.macro).to eq :belongs_to
+    end
+
+    it 'has many likes' do
+      relation = described_class.reflect_on_association(:likes)
+      expect(relation.macro).to eq :has_many
+    end
+
+    it 'has many comments' do
+      relation = described_class.reflect_on_association(:comments)
+      expect(relation.macro).to eq :has_many
+    end
+  end
 end
